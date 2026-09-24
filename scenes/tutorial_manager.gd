2d84fd3ad62c2ab27c776d56f038f4063c489790
Maximilian Misiurski <maxtherock@proton.me>
2026-09-24 16:10:17 +0100

    quick value refining

--- scenes/tutorial_manager.gd
+++ scenes/tutorial_manager.gd
@@ -20,17 +20,17 @@ func setup(minty_old_position, minty_position, focus):
 
 func main_tutorial():
 	show()
-	setup(Vector2(573.0,364.0), Vector2(573.0,364.0), "top_bar")
+	setup(Vector2(673.0,364.0), Vector2(673.0,364.0), "top_bar")
 
 func focus_changer():
 	if current_focus == "top_bar":
-		setup(Vector2(573.0,364.0), Vector2(573.0,364.0), "top_bar_level")
+		setup(Vector2(673.0,364.0), Vector2(473.0,364.0), "top_bar_level")
 	elif current_focus == "top_bar_level":
-		setup(Vector2(573.0,364.0), Vector2(973.0,364.0), "top_bar_money")
+		setup(Vector2(473.0,364.0), Vector2(473.0,364.0), "top_bar_money")
 	elif current_focus == "top_bar_money":
-		setup(Vector2(573.0,364.0), Vector2(473.0,364.0), "top_bar_book")
+		setup(Vector2(473.0,364.0), Vector2(473.0,364.0), "top_bar_book")
 	elif current_focus == "top_bar_book":
-		setup(Vector2(473.0,364.0), Vector2(573.0,364.0), "top_bar_clock")
+		setup(Vector2(473.0,364.0), Vector2(833.0,364.0), "top_bar_clock")
 	elif current_focus == "top_bar_clock":
-		setup(Vector2(573.0,364.0), Vector2(573.0,364.0), "none")
+		setup(Vector2(833.0,364.0), Vector2(573.0,364.0), "none")
 		hide()
