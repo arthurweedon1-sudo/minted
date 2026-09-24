a459db0fc6df3ea23881a0601ffcc31c0abbde75
Maximilian Misiurski <maxtherock@proton.me>
2026-09-24 11:06:52 +0100

    tutorial glide

--- scenes/tutorial_manager.gd
+++ scenes/tutorial_manager.gd
@@ -12,23 +12,25 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	pass
 
-func setup(minty_position, focus):
-	%minty.position = minty_position
+func setup(minty_old_position, minty_position, focus):
+	%minty.position = minty_old_position
+	var tween = create_tween()
+	tween.tween_property(%minty, "position", minty_position, 0.5)
 	current_focus = focus
 
 func main_tutorial():
 	show()
-	setup(Vector2(573.0,364.0), "top_bar")
+	setup(Vector2(573.0,364.0), Vector2(573.0,364.0), "top_bar")
 
 func focus_changer():
 	if current_focus == "top_bar":
-		setup(Vector2(573.0,364.0), "top_bar_level")
+		setup(Vector2(573.0,364.0), Vector2(573.0,364.0), "top_bar_level")
 	elif current_focus == "top_bar_level":
-		setup(Vector2(973.0,364.0), "top_bar_money")
+		setup(Vector2(573.0,364.0), Vector2(973.0,364.0), "top_bar_money")
 	elif current_focus == "top_bar_money":
-		setup(Vector2(473.0,364.0), "top_bar_book")
+		setup(Vector2(573.0,364.0), Vector2(473.0,364.0), "top_bar_book")
 	elif current_focus == "top_bar_book":
-		setup(Vector2(973.0,364.0), "top_bar_clock")
+		setup(Vector2(473.0,364.0), Vector2(573.0,364.0), "top_bar_clock")
 	elif current_focus == "top_bar_clock":
-		setup(Vector2(973.0,364.0), "none")
+		setup(Vector2(573.0,364.0), Vector2(573.0,364.0), "none")
 		hide()
